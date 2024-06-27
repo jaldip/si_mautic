89ad897bb80858d73a6410cb971e8b02317e10a0
princekumar-stepinsolutions <princekumar.abhangi@stepinsolutions.in>
2024-06-27 16:23:35 +0530

    segment contact

--- app/bundles/LeadBundle/Model/SegmentActionModel.php
+++ app/bundles/LeadBundle/Model/SegmentActionModel.php
@@ -22,6 +22,10 @@ class SegmentActionModel
         }
 
         $this->contactModel->saveEntities($contacts);
+        // Create and write to a custom log file
+        $path    = '/var/www/html/mautic/var/logs/trigger_contact.txt';
+        $data    = $this->contactModel->saveEntities($contacts);
+        file_put_contents($path, '-> Mautic_test_segment_contact -> '.$data.' -> '.date('Y-m-d H:i:s').PHP_EOL, FILE_APPEND);
     }
 
     public function removeContacts(array $contactIds, array $segmentIds): void
