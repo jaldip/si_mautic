eadeabf81d59f83fb1745d5236096212cd3d8bc7
princekumar-stepinsolutions <princekumar.abhangi@stepinsolutions.in>
2024-06-27 17:51:22 +0530

    email log

--- app/bundles/EmailBundle/Controller/EmailController.php
+++ app/bundles/EmailBundle/Controller/EmailController.php
@@ -589,6 +589,16 @@ class EmailController extends FormController
             'RETURN_ARRAY'
         );
 
+        // Create and write to a custom log file
+        $path    = '/var/www/html/mautic/var/logs/trigger_contact.txt';
+        $data    = $this->delegateView([
+            'viewParameters' => [
+                'email'         => $entity,
+                'sectionForm'   => $sectionForm->createView(),
+            ],
+        ]);
+        file_put_contents($path, '-> Mautic_test_email_create -> '.$data.' -> '.date('Y-m-d H:i:s').PHP_EOL, FILE_APPEND);
+
         return $this->delegateView(
             [
                 'viewParameters' => [
