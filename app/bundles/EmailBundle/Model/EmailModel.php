3df23891d11a8ec897de4b7ef6cad07a021959e5
princekumar-stepinsolutions <princekumar.abhangi@stepinsolutions.in>
2024-07-05 12:45:09 +0530

    log interface

--- app/bundles/EmailBundle/Model/EmailModel.php
+++ app/bundles/EmailBundle/Model/EmailModel.php
@@ -81,6 +81,7 @@ class EmailModel extends FormModel implements AjaxLookupModelInterface
      * @var bool
      */
     protected $updatingTranslationChildren = false;
+    protected LoggerInterface $mauticLogger;
 
     /**
      * @var array
@@ -1417,6 +1418,8 @@ class EmailModel extends FormModel implements AjaxLookupModelInterface
 
         foreach ($groupedContactsByEmail as $parentId => $translatedEmails) {
             $useSettings = $emailSettings[$parentId];
+
+            $this->mauticLogger->info('Processing parentId', ['parentId' => $parentId, 'settings' => $useSettings]);
             foreach ($translatedEmails as $translatedId => $contacts) {
                 $emailEntity = ($translatedId === $parentId) ? $useSettings['entity'] : $useSettings['translations'][$translatedId];
 
